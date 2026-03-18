5e10bec964f9991a153305123b80e40bfa3ba4b1
Harsha D <d2876379@gmail.com>
2026-03-18 16:50:19 +0530

    Add reconnection options to socket connection

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,10 +1,11 @@
-// src/socket.js
 import { io } from "socket.io-client";
 
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:5000";
 
 const socket = io(BACKEND_URL, {
-  transports: ["websocket", "polling"]
+  transports: ["websocket", "polling"],
+  reconnectionDelay: 1000,
+  reconnectionAttempts: 10,
 });
 
 export default socket;
