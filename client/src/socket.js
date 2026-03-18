6cc892e92690feb659a8430ecb9173268f8bfbf0
Harsha D <d2876379@gmail.com>
2026-03-18 16:44:55 +0530

    Update socket connection to use environment variable

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,10 @@
 // src/socket.js
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000"); // Your backend URL
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:5000";
+
+const socket = io(BACKEND_URL, {
+  transports: ["websocket", "polling"]
+});
 
 export default socket;
